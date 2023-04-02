7c3db864592102b1dbe75dd02c6ec8efc60029ab
Timophey <ulezko.2015@mail.ru>
2023-04-03 00:04:26 +0300

    extract constants

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/GitHubLinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/GitHubLinkParser.java
@@ -4,13 +4,17 @@ import ru.tinkoff.edu.java.linkparser.parsingresult.GitHubParsingResult;
 import ru.tinkoff.edu.java.linkparser.parsingresult.ParsingResult;
 
 public class GitHubLinkParser extends LinkParser {
+
+    private final String LINK_PREFIX = "https://github.com/";
+    private final int USER_STR_IDX = 0;
+    private final int REPO_STR_IDX = 1;
+
     @Override
     public ParsingResult parse(String url) {
-        if (url.startsWith("https://github.com")) {
-            String[] arr = url.split("/");
-            if (arr.length == 5) {
-                return new GitHubParsingResult(arr[3], arr[4]);
-            }
+        if (url.startsWith(LINK_PREFIX)) {
+            String s = url.substring(LINK_PREFIX.length(), url.length() - 1);
+            String[] arr = s.split("/");
+            return new GitHubParsingResult(arr[USER_STR_IDX], arr[REPO_STR_IDX]);
         }
         return nextParser.parse(url);
     }
--- link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/StackOverflowLinkParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/parser/StackOverflowLinkParser.java
@@ -4,13 +4,16 @@ import ru.tinkoff.edu.java.linkparser.parsingresult.ParsingResult;
 import ru.tinkoff.edu.java.linkparser.parsingresult.StackOverflowParsingResult;
 
 public class StackOverflowLinkParser extends LinkParser {
+
+    private final String LINK_PREFIX = "https://stackoverflow.com/questions/";
+    private final int QUESTION_ID_IDX = 0;
+
     @Override
     public ParsingResult parse(String url) {
-        if (url.startsWith("https://stackoverflow.com/questions/")) {
-            String[] arr = url.split("/");
-            if (arr.length == 6) {
-                return new StackOverflowParsingResult(Integer.parseInt(arr[4]));
-            }
+        if (url.startsWith(LINK_PREFIX)) { // 36
+            String s = url.substring(LINK_PREFIX.length(), url.length() - 1);
+            String[] arr = s.split("/");
+            return new StackOverflowParsingResult(Integer.parseInt(arr[QUESTION_ID_IDX]));
         }
         return nextParser.parse(url);
     }
