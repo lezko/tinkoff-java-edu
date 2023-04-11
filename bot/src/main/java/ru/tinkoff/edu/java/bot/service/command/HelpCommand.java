71015d380ddfe69c06bbfb1266928f691c6fd71b
Timophey <ulezko.2015@mail.ru>
2023-04-12 00:45:07 +0300

    autowire config

--- bot/src/main/java/ru/tinkoff/edu/java/bot/service/command/HelpCommand.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/service/command/HelpCommand.java
@@ -4,13 +4,18 @@ import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class HelpCommand implements Command {
 
     private List<Command> commands;
+    @Autowired
+    @Lazy
+    private CommandsConfiguration config;
 
     private void setupCommands() {
         commands = List.of(
@@ -34,12 +39,13 @@ public class HelpCommand implements Command {
 
     @Override
     public SendMessage handle(Update update) {
-        if (commands == null) {
-            setupCommands();
-        }
+//        if (commands == null) {
+//            setupCommands();
+//        }
+
         long chatId = update.message().chat().id();
         StringBuilder s = new StringBuilder();
-        for (Command c : commands) {
+        for (Command c : config.commands()) {
             s.append(c.command()).append(" - ").append(c.description()).append("\n");
         }
         return new SendMessage(chatId, s.toString());
