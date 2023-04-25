27390f014374c1527819178b501745b4836f41c1
Timophey <ulezko.2015@mail.ru>
2023-04-26 00:07:57 +0300

    add classes

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/database/accessconfiguration/JooqAccessConfiguration.java
@@ -0,0 +1,9 @@
+package ru.tinkoff.edu.java.scrapper.database.accessconfiguration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jooq")
+public class JooqAccessConfiguration {
+}
